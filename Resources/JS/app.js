162f5d1678bdb0988dbd93a1678232b214fee9c7
ffuffix <ffuffix@gmail.com>
2025-12-10 18:49:54 +0100

    fix0.2 for direct url

--- Resources/JS/app.js
+++ Resources/JS/app.js
@@ -4,20 +4,19 @@
 
     try {
         // Load home template
-        const homeResponse = await fetch('Resources/Templates/home.html');
+        const homeResponse = await fetch('/Resources/Templates/home.html');
         templates['/'] = await homeResponse.text();
 
         // Load art template
-        const artResponse = await fetch('Resources/Templates/art.html');
+        const artResponse = await fetch('/Resources/Templates/art.html');
         templates['/art'] = await artResponse.text();
 
         // Load contact template
-        const contactResponse = await fetch('Resources/Templates/contact.html');
+        const contactResponse = await fetch('/Resources/Templates/contact.html');
         templates['/contact'] = await contactResponse.text();
 
-        // Load 404 template
-        const notFoundResponse = await fetch('Resources/Templates/404.html');
-        templates['404'] = await notFoundResponse.text();
+        //const notFoundResponse = await fetch('Resources/Templates/404.html');
+        //templates['404'] = await notFoundResponse.text();
     } catch (error) {
         console.error('Error loading templates:', error);
         return;
@@ -26,7 +25,7 @@
     window.router.register('/', templates['/']);
     window.router.register('/art', templates['/art']);
     window.router.register('/contact', templates['/contact']);
-    window.router.register('404', templates['404']);
+    //window.router.register('404', templates['404']);
 
     window.router.handleNavigation();
 })();
